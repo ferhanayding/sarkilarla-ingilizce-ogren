e8cd916378a1352d070e9ba6fe21d7cdb5daf408
ferhanayding <ferhanaydin099@gmail.com>
2025-09-11 22:13:37 +0400

    feat: add About page with basic content and layout

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="h-[110dvh] flex items-end   p-10 justify-center ">
+      Tertemiziz Hocam. by Ferhan Aydın
+    </div>
+  );
+};
+
+export default About;
